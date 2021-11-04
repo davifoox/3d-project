aa6a39416839f99cff21f5ce284b9ebff91fc1db
Rubens Marreira <rubenslevimarreira@gmail.com>
2021-11-04 18:14:04 -0300

    Switch Camera

--- Assets/Scripts/Camera/SwitchCameras.cs
+++ Assets/Scripts/Camera/SwitchCameras.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class SwitchCameras : MonoBehaviour
 {
     
-    public GameObject[] cameras;
+    public GameObject player;
+    public Transform tFollow;
+    private CinemachineVirtualCamera vcam1;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        vcam1 = GetComponent<CinemachineVirtualCamera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            if(player!= null)
+            {
+                tFollow = player.transform;
+                vcam1.Follow = tFollow;
+
+            }
+
+        }
     }
 }
