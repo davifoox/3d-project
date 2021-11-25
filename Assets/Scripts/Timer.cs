c237e659e54f5666f3c017df5494fd3d45e5fda9
Rubens Marreira <rubenslevimarreira@gmail.com>
2021-11-25 11:54:20 -0300

    Fixing bugs 2

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -15,7 +15,7 @@ public class Timer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
