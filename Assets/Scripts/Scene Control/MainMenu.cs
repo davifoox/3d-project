c7d33ca3f83430eb05506e365e353e9de37f9ae5
Rubens Marreira <rubenslevimarreira@gmail.com>
2021-11-25 20:19:50 -0300

    Fixing

--- Assets/Scripts/Scene Control/MainMenu.cs
+++ Assets/Scripts/Scene Control/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     
     public void PlayGame()
     {
-        SceneManager.LoadScene("TutorialScene");
+        SceneManager.LoadScene("World01");
 
     }
 
