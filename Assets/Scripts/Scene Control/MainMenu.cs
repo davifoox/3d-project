7366cd89923d467116cc193adce9bea43ab3041e
Davi Fox <davifoox@gmail.com>
2021-11-25 17:51:27 -0300

    Update MainMenu.cs

--- Assets/Scripts/Scene Control/MainMenu.cs
+++ Assets/Scripts/Scene Control/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     
     public void PlayGame()
     {
-        SceneManager.LoadScene("World01");
+        SceneManager.LoadScene("TutorialScene");
 
     }
 
